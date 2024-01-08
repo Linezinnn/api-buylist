cb8b32c3dc738a109153a6a00672ca7c8d6b0793
Vinicius da Silva <vinisoliver20@gmail.com>
2024-01-08 11:08:19 -0300

    add CORS config

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,15 @@ import fastify, { FastifyInstance } from "fastify";
 
 import './database/prisma-client.ts'
 import { routesIndex } from "./routes/routes.js";
+import { fastifyCors } from "@fastify/cors";
 
 const server: FastifyInstance = fastify({ logger: true })
 const port: number = 8080
 
 server.register(routesIndex)
+server.register(fastifyCors, {
+    origin: 'http://localhost:5173'
+})
 
 server.listen({
     port
